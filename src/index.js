5e6ad9b083cf5d8fb52791a39d621639bbdbf6ea
AbeeraTahir <abeeratahir2018@gmail.com>
2022-12-12 20:32:25 +0500

    Structure the application files

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>,
 );
