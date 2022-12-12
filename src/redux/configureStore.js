9c757a7aac7ada392eea96ace7c0803c374ea7ce
AbeeraTahir <abeeratahir2018@gmail.com>
2022-12-12 23:45:23 +0500

    Add action and reducer for category

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
+import categoriesReducer from './categories/categories';
 
 const store = configureStore({
   reducer: {
     books: bookReducer,
+    categories: categoriesReducer,
   },
 });
 
